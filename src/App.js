9d79c9ee6b489dd6bf03bc88c724ef3df502d5c5
cocogueva <coco.guevara123@gmail.com>
2026-05-13 12:43:37 -0500

    Upload fonts to project

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import "@fontsource/bebas-neue";
+import "@fontsource/inter/300.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
 import React, { useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
